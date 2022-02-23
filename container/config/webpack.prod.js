4ec2c9fe2d25f87a1ae882eaac7e99f455de1138
Mayur Baldha <mayur.baldha@rapidops.com>
2022-02-23 18:47:40 +0530

    Changes for the dashboard

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -8,7 +8,8 @@ const domain = process.env.PRODUCTION_DOMAIN
 const prodConfig = {
     mode: "production",
     output: {
-        filename: '[name].[contecthash].js'
+        filename: '[name].[contecthash].js',
+        publicPath:'/container/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
