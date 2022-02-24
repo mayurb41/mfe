8a0cda688afdc5a17b8131aea8514807e13b4720
Mayur Baldha <mayur.baldha@rapidops.com>
2022-02-24 11:48:13 +0530

    CHanges for the header

--- container/src/App.js
+++ container/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import DashboardApp from './components/DashboardApp';
+import Header from './components/Header';
 
 
 export default () => {
   return (
-    <div>
-      <h1>Hi there!!!!!!!!!!</h1>
-      <hr />
-      <MarketingApp />
-      <DashboardApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+        <DashboardApp />
+      </div>
+    </BrowserRouter>
   );
 };
