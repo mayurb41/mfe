3d52959d72e213c441ca1aad3e24bfe16d1f8789
Mayur Baldha <mayur.baldha@rapidops.com>
2022-02-24 14:52:23 +0530

    CSS classnames added

--- container/src/App.js
+++ container/src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 import MarketingApp from './components/MarketingApp';
 import DashboardApp from './components/DashboardApp';
 import Header from './components/Header';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+})
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-        <DashboardApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+          <DashboardApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
